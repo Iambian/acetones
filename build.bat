597ffb4828fee3816f7cacf7b4eb91fc85188a2c
Iambian <None>
2024-11-03 19:49:14 -0500

    Claims to load the ROM. TODO: Set up debug mode for emulator.

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 @echo off
 if not exist "bin\" mkdir bin\
 tooling\spasm -E dbgtest.z80 bin\dbgtest.8xp
-tooling\spasm -E -T  main.z80 bin\ACETONES.8xp
+tooling\spasm -E -T -L main.z80 bin\ACETONES.8xp
