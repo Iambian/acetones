2f99c4b86d072b0ee668bd04c5111e5e76c7f999
Iambian <None>
2025-01-03 01:59:24 -0500

    Worked a bit more on the 2nd core

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 @echo off
 if not exist "bin\" mkdir bin\
 rem tooling\spasm -E dbgtest.z80 bin\dbgtest.8xp
-tooling\spasm -E -T -L main.z80 bin\ACETONES.8xp
-rem tooling\spasm -E core2.z80 F.8xp
+rem tooling\spasm -E -T -L main.z80 bin\ACETONES.8xp
+tooling\spasm -E core2.z80 bin\F.8xp
